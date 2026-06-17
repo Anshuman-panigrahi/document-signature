386cf57fe7b4677eee11b96073bf02099b9b9091
Anshuman Panigrahi <panigrahia067@gmail.com>
2026-06-17 23:50:32 +0530

    Fix Express 5 PathError by naming the options wildcard parameter

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.use(
   })
 );
 
-app.options("*", cors());
+app.options("/*splat", cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
