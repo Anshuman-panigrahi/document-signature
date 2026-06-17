ba2decc19c842d75eac34a6cb67a69c198a048eb
Anshuman Panigrahi <panigrahia067@gmail.com>
2026-06-17 10:24:02 +0530

    Fixed production backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://document-signature-backend-ozs5.onrender.com",
+  baseURL: "https://document-signature-backend-ozs5.onrender.com",
 });
 
 export default API;
